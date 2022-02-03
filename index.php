e082a8d8ddcf933391f4ac6f14aecebd60b2a13a
zile028 <zile028@gmail.com>
2022-02-03 16:03:19 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Auto-skola</title>
+    <!-- Font Awesome 5 | visit: https://fontawesome.com/v5.15/icons?d=gallery&p=2 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <!-------- top bar -------->
+    <section class="topBar">
+        <article class="container">
+            <ul>
+                <li><i class="fas fa-phone-alt"></i>1-800-123-6889</li>
+                <li><i class="fas fa-map-marker-alt"></i>8th floor, 379 Hudson St, New York</li>
+                <li><i class="far fa-clock"></i>09:30 am – 06:00 pm</li>
+            </ul>
+        </article>
+    </section>
+
+
+    <!-------- navbar -------->
+    <nav>
+        <article class="container">
+            <div class="log">
+                <a href="index.php"><img src="img/logo.png" alt=""></a>
+            </div>
+            <div class="navbar">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li>About</li>
+                    <li><a href="news.php">News</a></li>
+                    <li><a href="courses.php">Courses</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <li>
+                        <a href="/"><i class="fab fa-facebook-f"></i></a>
+                        <a href="/"><i class="fab fa-twitter"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </article>
+    </nav>
+
+
+    <!-------- header -------->
+    <header>
+        <img src="img/slide-03.jpg" alt="">
+        <article>
+            <h1>free video lessons</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur incidunt perspiciatis quo, fuga
+                dolor vitae assumenda vel magni eaque natus?</p>
+            <div>
+                <a class="btn" href="courses.php">our services</a>
+                <a class="btn btnLight" href="teachers.php">our teacher</a>
+            </div>
+        </article>
+    </header>
+
+
+
+</body>
+
+</html>
